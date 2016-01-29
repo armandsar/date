a53f3777bc6ffe7c85d6adaf41b79c1fc8d34806
armandsar <armands.leinieks@gmail.com>
2016-01-30 01:21:07 +0200

    add part of LV translation

---
+++ src/Lang/lv.php
@@ -0,0 +1,48 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Date Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the date library. Each line can
+    | have a singular and plural translation separated by a '|'.
+    |
+    */
+
+    'ago'       => ':time ago',
+    'from_now'  => ':time from now',
+    'after'     => ':time after',
+    'before'    => ':time before',
+    'year'      => '1 year|:count years',
+    'month'     => '1 month|:count months',
+    'week'      => '1 week|:count weeks',
+    'day'       => '1 day|:count days',
+    'hour'      => '1 hour|:count hours',
+    'minute'    => '1 minute|:count minutes',
+    'second'    => '1 second|:count seconds',
+
+    'january'   => 'janvāris',
+    'february'  => 'februāris',
+    'march'     => 'marts',
+    'april'     => 'aprīlis',
+    'may'       => 'maijs',
+    'june'      => 'jūnijs',
+    'july'      => 'jūlijs',
+    'august'    => 'augusts',
+    'september' => 'septembris',
+    'october'   => 'oktrobris',
+    'november'  => 'novembris',
+    'december'  => 'decembris',
+
+    'monday'    => 'pirmdiena',
+    'tuesday'   => 'otrdiena',
+    'wednesday' => 'trešdiena',
+    'thursday'  => 'ceturtdiena',
+    'friday'    => 'piektdiena',
+    'saturday'  => 'sestdiena',
+    'sunday'    => 'svētdiena',
+
+);
